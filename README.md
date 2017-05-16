2459118c3a10e000515468165e548cfc43cfd2d1
Corentin Rondier <contact@rondier.io>
2017-05-16 19:16:53 +0200

    Initial Commit
    It doesn't work, but i'm on it

---
+++ README.md
@@ -0,0 +1,27 @@
+# WikiBot
+A Discord bot that search things on Wikipedia
+
+WikiBot is a Discord Bot using the Discord.py API Wrapper and the Wikipedia Wrapper
+
+## Test
+
+You can invite the WikiBot on your server simply by [Going to this link](https://discordapp.com/api/oauth2/authorize?client_id=313646227394920448&scope=bot&permissions=0)
+
+You can also join the official [Discord server](https://discord.gg/9M8rMe6)
+
+## Requirements
+
+- `Python 3.4.2+` (Tested on Python 3.6.1 and 3.4.2)
+- [Discord.py](https://github.com/Rapptz/discord.py)
+- [Wikipedia](https://github.com/goldsmith/Wikipedia)
+
+## Installation
+
+First of, make sure that you have all the requirements
+(You can just type `pip install Wikipedia discord.py`)
+
+Then, you need to get register a bot on the [Discord Developer Page](https://discordapp.com/developers/applications/me)
+When you have your bot, change the value of the `token` variable in the source code
+
+Finally, just run your bot. He's alive!
+To add it in your own server, just create his OAuth2 invite link
---
+++ WikiBot.py
@@ -0,0 +1,107 @@
+import discord    
+import asyncio
+import wikipedia
+
+language = "en"
+
+client = discord.Client()
+
+@client.event
+@asyncio.coroutine
+def on_ready():
+    print("Bot is ready")
+    print(client.user.name)
+    print(client.user.id)
+
+token = "Your token"
+
+@client.event
+@asyncio.coroutine
+def on_server_join(server):
+    yield from client.send_message(server.default_channel, "Oi, i'm the WikiBot! https://en.wikipedia.org/wiki/Main_Page")
+
+    
+@client.event
+@asyncio.coroutine
+def on_message(message):
+    if message.channel.is_private and message.author.id != client.user.id:
+        wikipage = None
+        lookup = True
+
+        try:
+            wikipage = wikipedia.page(query)
+            print("I found directly")            
+                
+        except wikipedia.exceptions.PageError:
+            print("Can't access by default. Trying to search")
+            
+        except Exception:
+            lookup = False
+                
+        if wikipage == None and lookup:
+            wikipage = wikipedia.suggest(query)
+            
+        if wikipage == None and lookup:
+            yield from client.send_message(message.channel, "Sorry, cannot find " + query + " :v")
+        elif (not lookup):
+            yield from client.send_message(message.channel, "Something went wrong. Try to be more specific in search, or maybe I can't reach Wikipedia")
+        else:
+            imglist = wikipage.images
+            if len(imglist) == 0:
+                em = discord.Embed(title=wikipage.title, description=wikipedia.summary(query, sentences=2), colour=0x2DAAED, url=wikipage.url)
+            else:
+                em = discord.Embed(title=wikipage.title, description=wikipedia.summary(query, sentences=2), colour=0x2DAAED, url=wikipage.url, image=imglist[0])
+                em.set_author(name=client.user.name, icon_url="https://wikibot.rondier.io")
+                yield from client.send_message(message.channel, embed=em)
+                yield from client.send_message(message.channel, "More at " + wikipage.url)
+                
+
+
+    else:
+        ping = "<@" + client.user.id + ">"
+        if message.content.startswith(ping):
+        
+            print("I'm called!")
+            
+            toretract = len(ping)
+            query = message.content[toretract:]
+            
+            if query[0] == " ":
+                query = query[1:]
+            
+            print("Query = " + query)
+            
+            wikipage = None
+            
+            lookup = True
+            
+            try:
+                wikipage = wikipedia.page(query)
+                print("I found directly")            
+                
+            except wikipedia.exceptions.PageError:
+                print("Can't access by default. Trying to search")
+            
+            except Exception:
+                lookup = False
+                
+            if wikipage == None and lookup:
+                wikipage = wikipedia.suggest(query)
+            
+            if wikipage == None and lookup:
+                yield from client.send_message(message.channel, "Sorry, cannot find " + query + " :v")
+            elif (not lookup):
+                yield from client.send_message(message.channel, "Something went wrong on Wikipedia's side, try again later")
+            else:
+                imglist = wikipage.images
+                if len(imglist) == 0:
+                    em = discord.Embed(title=wikipage.title, description=wikipedia.summary(query, sentences=2), colour=0x2DAAED, url=wikipage.url)
+                else:
+                    em = discord.Embed(title=wikipage.title, description=wikipedia.summary(query, sentences=2), colour=0x2DAAED, url=wikipage.url, image=imglist[0])
+                em.set_author(name=client.user.name, icon_url="https://wikibot.rondier.io")
+                yield from client.send_message(message.channel, embed=em)
+                yield from client.send_message(message.channel, "More at " + wikipage.url)
+                
+            
+
+client.run(token)
